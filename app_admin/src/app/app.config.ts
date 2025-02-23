e3e141a08f3c34a30c4671be4e071cc830e7bbc3
Robert Lulashi <robert.lulashi@gmail.com>
2025-02-23 13:48:55 -0500

    Module 7 Completed Baseline

--- app_admin/src/app/app.config.ts
+++ app_admin/src/app/app.config.ts
@@ -1,11 +1,15 @@
-import { ApplicationConfig, provideZoneChangeDetection } from "@angular/core";
+import { ApplicationConfig, provideZoneChangeDetection, importProvidersFrom } from "@angular/core";
 import { provideRouter } from "@angular/router";
-import { provideHttpClient } from "@angular/common/http";
+import { HttpClientModule, provideHttpClient } from "@angular/common/http";
 
 import { routes } from "./app.routes";
+import { authInterceptProvider } from "./utils/jwt.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()
-    
+  providers: [
+    provideRouter(routes), 
+    provideHttpClient(),
+    importProvidersFrom(HttpClientModule),
+    authInterceptProvider    
   ]
 };
